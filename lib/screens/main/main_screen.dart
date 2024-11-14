f5fbb241d6f8d3fb84184ec2a34bc4a92c12d7ad
Suki <tues1205@naver.com>
2024-11-14 16:24:02 +0900

    feat: 최근게시물 Carousel 구현

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -7,7 +8,10 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
+        backgroundColor: Colors.white,
+        appBar: AppBar( // 상단바
+          scrolledUnderElevation: 0,
+          backgroundColor: Colors.white,
           title: _logoImage(),
           centerTitle: true, // 중앙 배치
           actions: [
@@ -18,17 +22,18 @@ class MainScreen extends StatelessWidget {
           ],
         ),
         body: SingleChildScrollView(
-          child: Center (
-            child: Column(
-                children: [
-                  _searchBar(),
-                  SizedBox(height: 18),
-                  _mainContent()
-                ]
-            ),
+          child: Column(
+              children: [
+                SizedBox(height: 5),
+                _searchBar(), // 검색바
+                SizedBox(height: 18),
+                _mainContent(), // 중앙콘텐츠
+                SizedBox(height: 50),
+                _recentPosts(), // 최근게시물
+              ]
           ),
-
         ),
+        bottomNavigationBar: BottomAppBar(), // 하단바
       ),
     );
   }
@@ -59,7 +64,7 @@ class MainScreen extends StatelessWidget {
   // 검색 바
   Widget _searchBar() {
     return Container(
-      width: 360,
+      width: 380,
       height: 35,
       decoration: BoxDecoration(
         color: Color(0x264B7E5B), // 투명도15%
@@ -104,4 +109,83 @@ class MainScreen extends StatelessWidget {
       ],
     );
   }
+
+  // 최근 게시물
+  Widget _recentPosts() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Row(
+            children: [
+              Text(
+                '최근 게시물',
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+              ),
+              Spacer(),
+              Icon(Icons.arrow_forward, size: 18), // 아이콘 추가
+            ],
+          ),
+        ),
+        SizedBox(height: 10),
+        _makeCarousel(), // 캐러셀
+      ],
+    );
+  }
+
+  // 케러셀
+  Widget _makeCarousel() {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child : CarouselSlider(
+          options: CarouselOptions(
+            height: 190, // 높이 설정
+            enlargeCenterPage: false, // 페이지 확대 안함
+            enableInfiniteScroll: false, // 무한스크롤 안함
+            autoPlay: false, // 자동슬라이드 안함
+            initialPage: 1,
+            viewportFraction: 0.4, // 각 게시물간 간격
+          ),
+
+          items: [ // 예시 게시물
+            _postItem('팥이', '몬스테라', 'assets/images/sample_post.png'),
+            _postItem('콩이', '레몬나무', 'assets/images/sample_post.png'),
+            _postItem('그루트', '금전수', 'assets/images/sample_post.png'),
+            _postItem('팥이', '몬스테라', 'assets/images/sample_post.png'),
+            _postItem('콩이', '레몬나무', 'assets/images/sample_post.png'),
+          ],
+        ),
+    );
+  }
+
+  // 각 게시물
+  Widget _postItem(String name, String species, String imageUrl) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              imageUrl,
+              width: 130,
+              height: 130,
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            species,
+            style: TextStyle(fontSize: 9, color: Color(0xFF757575)),
+          ),
+          Text(
+            name,
+            style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
